b385f25e1d20563892140854d69a26117f04b06b
Sajan-Kumar-Sahu <saajansahu.3007@gmail.com>
2024-06-14 08:15:48 +0530

    O(logN)

---
+++ src/main/java/Arrays/RotatedSortedArray.java
@@ -0,0 +1,54 @@
+/* There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+Example 3:
+Input: nums = [1], target = 0
+Output: -1 */
+package Arrays;
+
+public class RotatedSortedArray {
+    public static void main(String[] args) {
+        int arr[] = {5,1,3};
+        int target = 3;
+        int location = searchElement(arr, 0, arr.length - 1, target);
+        System.out.println(location);
+    }
+
+    public static int searchElement(int[] arr, int start, int end, int target) {
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            }
+            if (arr[start] <= arr[mid]) {
+                if (arr[start] <= target && target <= arr[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else {
+                if (arr[mid] <= target && target <= arr[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
